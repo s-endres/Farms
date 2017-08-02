e94bf795a3eb11d86f599e84bee7a200314c03d9
Stephan Endres <stephan.endres@outlook.com>
2017-08-02 17:21:09 -0600

    index and create of parcel done

---
+++ Farms/Controllers/ParcelController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Farms.Data;
+using Farms.Models;
+
+namespace Farms.Controllers
+{
+    public class ParcelController : Controller
+    {
+        // GET: Parcel
+        public ActionResult Index()
+        {
+            return View();
+        }// GET: Parcel
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAllParcelByFarmId(int farmId)
+        {
+            return Json(ParcelData.ParcelList.Where(p => p.IdFarm == farmId), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetAllConditions()
+        {
+            return Json(ParcelData.ConditionList, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult AddParcel([Bind(Include = "Size,Description,IdFarm,Observations,ConditionIds")] Parcel parcel)
+        {
+            if (!string.IsNullOrEmpty(parcel.Size.ToString()) && !string.IsNullOrEmpty(parcel.Description) && parcel.Observations.Count > 0)
+            {
+                parcel.PlantIds = new List<int>();
+                var last = ParcelData.ParcelList.Last();
+                parcel.Id = last.Id + 1;
+                ParcelData.ParcelList.Add(parcel);
+                return Json(parcel, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Farms/Data/ParcelData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Farms.Models;
+
+namespace Farms.Data
+{
+    public static class ParcelData
+    {
+        public static List<Parcel> ParcelList = new List<Parcel>()
+        {
+            new Parcel()
+            {
+                Id = 1,
+                Size = 15.98,
+                Description = "asdasdasdasdasdasdasd",
+                IdFarm = 1,
+                PlantIds = new List<int>(){1,2},
+                ConditionIds = new List<int>(){3,4}
+            },
+            new Parcel()
+            {
+                Id = 2,
+                Size = 176.98,
+                Description = "qwe qwe qweqw eqwe qwe",
+                IdFarm = 1,
+                PlantIds = new List<int>(){1,2},
+                ConditionIds = new List<int>(){3,4}
+            }
+        };
+
+        public static List<Condition> ConditionList = new List<Condition>()
+        {
+            new Condition()
+            {
+                Id = 1,
+                Name = "Rainy"
+            },
+            new Condition()
+            {
+                Id = 2,
+                Name = "Dry"
+            },
+            new Condition()
+            {
+                Id = 3,
+                Name = "Very dry"
+            }
+        };
+
+
+
+    }
+}
