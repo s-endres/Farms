94ca587846935280ffa641882080a4f5234be64c
Ogeid13 <diego.rodr.gonz.96@gmail.com>
2017-08-10 14:02:45 -0700

    Farms finished, second crud started

---
+++ Farms/Scripts/Basics methods.js
@@ -0,0 +1,21 @@
+﻿function addAndRemoveClassByIdElem(elementIdentifier, classToAdd, classToRemove) {
+    $(elementIdentifier).addClass(classToAdd).removeClass(classToRemove);
+}
+
+function ShowHide(tr, closeIt) {
+    var row = $table.row(tr);
+
+    if (row.child.isShown() || closeIt) {
+        //Child is opened, so close it
+        row.child.hide();
+        tr.removeClass("shown");
+    } else {
+        //Child is closed, so open it
+        row.child(ExtractInfo()).show();
+        tr.addClass("shown");
+    }
+}
+
+function ModifyDeleteModal(index) {
+    $("#IdDeleter").val(index);
+}
